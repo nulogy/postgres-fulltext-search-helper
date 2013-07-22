e61fbb95c4ba69dff2798684bf45c6c2c59395d9
Steph <stephaniem@nulogy.com>
2013-07-22 12:58:02 -0400

    matches the second half of a hyphenated word/number

--- spec/postgres-fulltext-search-helper_spec.rb
+++ spec/postgres-fulltext-search-helper_spec.rb
@@ -6,24 +6,24 @@ describe PostgresFulltextSearchHelper do
   it "returns a string that can be used by postgres" do
     query = "hello"
 
-    expect(Helper.format_query_for_fulltext(query)).to eq("hello:*")
+    expect(Helper.format_query_for_fulltext(query)).to eq("(hello:* | -hello:*)")
   end
 
   it "whitespace is interpreted as &" do
     query = "hello world"
 
-    expect(Helper.format_query_for_fulltext(query)).to eq("hello:* & world:*")
+    expect(Helper.format_query_for_fulltext(query)).to eq("(hello:* | -hello:*) & (world:* | -world:*)")
   end
 
   it "handles sensitive characters" do
     query = "*:hello &(world)!|"
-    expect(Helper.format_query_for_fulltext(query)).to eq("hello:* & world:*")
+    expect(Helper.format_query_for_fulltext(query)).to eq("(hello:* | -hello:*) & (world:* | -world:*)")
   end
 
   it "adds a filter to a activerecord-style scope" do
     scope = stub("scope").as_null_object
 
-    scope.should_receive(:where).with("to_tsvector('simple', name::text) @@ to_tsquery('simple', ?)", ["hello:* & world:*"])
+    scope.should_receive(:where).with("to_tsvector('simple', name::text) @@ to_tsquery('simple', ?)", ["(hello:* | -hello:*) & (world:* | -world:*)"])
 
     Helper.search(scope, "name", "hello world")
   end
